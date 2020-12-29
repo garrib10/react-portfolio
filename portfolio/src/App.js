3b30e703a623642033c24bf991561613806cb5da
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-29 17:52:57 -0500

    updated app.js and index.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "../src/Pages/Home";
-import Portfolio from "../src/Pages/Portfolio";
-import Contact from "../src/Pages/Contact";
+//import { BrowserRouter as Router, Route } from "react-router-dom";
+//import Home from "../src/Pages/Home";
+//import Portfolio from "../src/Pages/Portfolio";
+//import Contact from "../src/Pages/Contact";
+//import Header from "../src/components/Header";
 //import Navbar from "./components/NavBar";
 //import Footer from "./components/Footer";
-//import Wrapper from "./components/Wrapper";
+///import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <Router>
-      <div>
-        
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Home" component={Home} />
-          <Route exact path="/Portfolio" component={Portfolio} />
-          <Route exact path="/Contact" component={Contact} />
-        
-        
-      </div>
-    </Router>
+    <div>
+ 
+
+    </div>
   );
 }
 
