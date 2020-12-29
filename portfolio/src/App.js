b0891e66b54c9fc8ece6b19d540ddd82b7b52ad7
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-29 17:40:20 -0500

    see if this works

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Portfolio from "./Pages/Portfolio";
-import Home from "./Pages/Home";
-import Contact from "./Pages/Contact";
-import Navbar from "./components/NavBar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import Home from "../src/Pages/Home";
+import Portfolio from "../src/Pages/Portfolio";
+import Contact from "../src/Pages/Contact";
+//import Navbar from "./components/NavBar";
+//import Footer from "./components/Footer";
+//import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
-        <Wrapper>
+        
           <Route exact path="/" component={Home} />
           <Route exact path="/Home" component={Home} />
           <Route exact path="/Portfolio" component={Portfolio} />
           <Route exact path="/Contact" component={Contact} />
-        </Wrapper>
-        <Footer />
+        
+        
       </div>
     </Router>
   );
