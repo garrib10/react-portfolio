f263454d318e3c415a0e33014445fd5b74096d5b
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-29 17:04:53 -0500

    font awesome added to package-lock

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Portfolio from "./pages/Portfolio";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
+import Portfolio from "./Pages/Portfolio";
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -14,9 +14,9 @@ function App() {
         <Navbar />
         <Wrapper>
           <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/Portfolio" component={Portfolio} />
+          <Route exact path="/Contact" component={Contact} />
         </Wrapper>
         <Footer />
       </div>
