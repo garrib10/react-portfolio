46bbcafe99d58cf03f376d38a3ffeea91b01b434
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-29 20:09:58 -0500

    added AboutMe ccomponent

---
+++ portfolio/src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Picture from "../../Images/AboutMe"
+
+function AboutMe() {
+    return (
+        <div class="card" id="about-me">
+            <div className="about-me">
+            </div>
+            <div class="row">
+                <div class="card-body">
+                    <img src={Picture} className="float-left" id="profile-pic" alt="About Me" />
+                </div>
+                <div className>
+                    <p className="about-me-paragraph">
+                        <p>
+                            My name is Brian Garrity. I am a Connecticut native born and raised in a small town called Coventry.
+                            I just moved back about a month ago while living in Chicago for two years. I worked in the restaurant industry
+                            while living there and got a chance to work at a Michelin Star restaurant after my first year living there.
+                            I love trying new restaurants, cooking, and pretty much anything that has to do with food.
+                        </p>
+                        <p>
+                            Currently, I am taking the UConn Full Stack Development Bootcamp. When I complete the program, I will be
+                            pursing a career in web development. I have obtained a lot of skills along the way during the bootcamp, and
+                            here are the some of them.
+                    </p>
+
+                        <p><u>Skills</u>
+                            <span class="badge badge-pill badge-dark">API</span>
+                            <span class="badge badge-pill badge-dark">AJAX</span>
+                            <span class="badge badge-pill badge-dark">Bootstrap</span>
+                            <span class="badge badge-pill badge-dark">CSS</span>
+                            <span class="badge badge-pill badge-dark">DOTENV</span>
+                            <span class="badge badge-pill badge-dark">Express</span>
+                            <span class="badge badge-pill badge-dark">Express-handlebars</span>
+                            <span class="badge badge-pill badge-dark">HTML</span>
+                            <span class="badge badge-pill badge-dark">Javascript</span>
+                            <span class="badge badge-pill badge-dark">JawsDB</span>
+                            <span class="badge badge-pill badge-dark">Jquery</span>
+                            <span class="badge badge-pill badge-dark">JSON</span>
+                            <span class="badge badge-pill badge-dark">Moment.js</span>
+                            <span class="badge badge-pill badge-dark">MySql</span>
+                            <span class="badge badge-pill badge-dark">Node.js</span>
+                            <span class="badge badge-pill badge-dark">ORM</span>
+                            <span class="badge badge-pill badge-dark">Sequelize</span>
+                            <span class="badge badge-pill badge-dark">MongoDB</span>
+                        </p>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default AboutMe;
