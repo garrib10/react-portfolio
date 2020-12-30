ea96fb3d2b9e9d28e9c68b39b39fa21ff410677f
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-29 20:35:50 -0500

    updated app.css and wrapper component

--- portfolio/src/Pages/About.js
+++ portfolio/src/Pages/About.js
@@ -1,11 +1,26 @@
-import React  from "react";
+import React, {Component}  from "react";
+import "../App.css";
+//import AboutMe from "../components/AboutMe/AboutMe.js"
+import Header from "../components/Header";
+import Wrapper from "../components/Wrapper"
 
-function About() {
+
+
+class App extends Component {
+ 
+
+    render() {
     return (
-        <div>
-
-
-            
-        </div>
-    )
-}
+    <main>
+      <div className="App"> 
+          <Wrapper>
+          <Header/>
+          
+          </Wrapper>
+         </div>
+    </main>
+    );
+  }
+  }
+  export default App;
+  
