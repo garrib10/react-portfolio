1b0a0e96a5a26da660b9ba69560dd2db5959b38a
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-31 14:36:04 -0500

    starting fresh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./Pages/About";
-import Portfolio from "./Pages/Portfolio";
-import Contact from "./Pages/Contact";
-import Navbar from "./Components/NavBar";
-//import Footer from "./Components/Footer";
-//import Wrapper from "./Components/Wrapper";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-
-        <Route exact path="/" component={About} />
-        <Route exact path="/Portfolio" component={Portfolio} />
-        <Route exact path="/Contact" component={Contact} />
-      </div>
-      </Router>
-    
-  
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
