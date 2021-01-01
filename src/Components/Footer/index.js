6e6fd96bc5cea9108b30f20274a30aa1663e35d1
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-31 23:01:15 -0500

    added footer details just have to find out how to add them

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -5,8 +5,19 @@ function Footer() {
   return (
     <footer className="footer">
       <p>Created by Brian Garrity </p>
+
     </footer>
   );
 }
 
 export default Footer;
+
+//<h2><u>Resources</u>:
+//<a href="https://github.com/garrib10" target="_blank"><i class="fa fa-github"></i></a>
+//<a href="https://docs.google.com/document/d/1gcNMeLkQet_GUg4J4n8ur233EdAXZiGUi3Av-Y8z7lk/edit?usp=sharing"
+// target="_blank"><i class="far fa-file w3-hover-opacity"></i></a>
+//<a href="https://www.linkedin.com/in/briangarrity26/" target="_blank"><i
+  //  class="fab fa-linkedin w3-hover-opacity"></i></a>
+//<a href="https://dribbble.com/BGarrity26" target="_blank"><i class="fas fa-basketball-ball"></i></a>
+//<u>Email</u>:<a href="mailto:someone@example.com">garrib10@gmail.com</a> 
+ // <u>Phone Number</u>: 860-331-7857</h2>
