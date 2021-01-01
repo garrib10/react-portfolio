20d78cca84dd065ab85c8f6abccee0a7e2337cc5
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-31 22:57:30 -0500

    updated about js with badge pill for skills

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,7 +5,7 @@ function About() {
     <section className="about" id="about">
       <div className="container">
         <div className="about-me">
-          <h1>About Me</h1>
+          <h1><u>About Me</u></h1>
           <p> My name is Brian Garrity. I am a Connecticut native born and raised in a small town called Coventry.
           I just moved back about a month ago while living in Chicago for two years. I worked in the restaurant industry
           while living there and got a chance to work at a Michelin Star restaurant after my first year living there.
@@ -15,7 +15,26 @@ function About() {
           pursing a career in web development. I have obtained a lot of skills along the way during the bootcamp, and
           here are the some of them.
           </p>
-
+          <p><u> Skills:</u>
+          <span class="badge badge-pill badge-dark">API</span>
+          <span class="badge badge-pill badge-dark">AJAX</span>
+          <span class="badge badge-pill badge-dark">Bootstrap</span>
+          <span class="badge badge-pill badge-dark">CSS</span>
+          <span class="badge badge-pill badge-dark">DOTENV</span>
+          <span class="badge badge-pill badge-dark">Express</span>
+          <span class="badge badge-pill badge-dark">Express-handlebars</span>
+          <span class="badge badge-pill badge-dark">HTML</span>
+          <span class="badge badge-pill badge-dark">Javascript</span>
+          <span class="badge badge-pill badge-dark">JawsDB</span>
+          <span class="badge badge-pill badge-dark">Jquery</span>
+          <span class="badge badge-pill badge-dark">JSON</span>
+          <span class="badge badge-pill badge-dark">Moment.js</span>
+          <span class="badge badge-pill badge-dark">MySql</span>
+          <span class="badge badge-pill badge-dark">Node.js</span>
+          <span class="badge badge-pill badge-dark">ORM</span>
+          <span class="badge badge-pill badge-dark">Sequelize</span>
+          <span class="badge badge-pill badge-dark">MongoDB</span>
+          </p>
         </div>
       </div>
     </section>
