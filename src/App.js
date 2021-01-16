61cc41d27969e55494ec310ff9007b40a668809c
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-01-16 03:13:42 -0500

    navbar working properly

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
-import Nav from "./Component/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./Component/NavBar";
 import Footer from "./Component/Footer"
 import About from "./Pages/About"
 import Portfolio from "./Pages/Portfolio/Portfolio"
 import Contact from "./Pages/Contact"
+import Home from "./Pages/Home"
+
 
 
 function App() {
   return (
     <Router>
-    <div className="App">
-     <Nav/>
-            <Switch>
-              <Route exact path="/" component={About} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/portfolio" component={Portfolio} />
-              <Route exact path="/contact" component={Contact} />
-            </Switch>
-    </div>
-    <Footer/>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route extact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </div>
+      <Footer />
     </Router>
   );
 }
--- src/Component/NavBar/index.js
+++ src/Component/NavBar/index.js
@@ -14,8 +14,13 @@ function Navbar() {
         <nav>
           <div>
             <ul className="nav nav-tabs">
-              <li className="nav-item">
+            <li className="nav-item">
                 <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>  
+                  Home
+                </Link>
+                </li>
+              <li className="nav-item">
+                <Link to="/about" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>  
                   About
                 </Link>
               </li>
