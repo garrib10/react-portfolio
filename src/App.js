c401fa48a7a3d5ce2c8d49474513f7d79cba9b4b
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-02-09 14:52:55 -0500

    updated portfolio

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="App">
         <Header />
         <Switch> 
-          <Route expact path="/" component={About} />
+          <Route exact path="/"  component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
